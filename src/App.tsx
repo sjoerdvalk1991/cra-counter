b71f56f0c8b34b44cae11d8ac6239b48075b88aa
Sjoerd Valk <sjoerdvalk@Sjoerds-MacBook-Pro.local>
2022-05-04 11:54:14 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AFewHundredTimers from "./AFewHundredTimers";
+import AnotherFewHundredTimers from "./AnotherFewHundredTimers";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count} hello world!!!
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AFewHundredTimers />
+        <AnotherFewHundredTimers />
+
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
       </header>
     </div>
   );
